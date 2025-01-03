55b00f9a77205d5eda1f834593ca7ad0d2434776
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-01-03 16:27:14 +0000

    a commit to show how wordy it gets like this

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 const registers = @import("registers.zig");
+const opcodes = @import("opcodes.zig");
+const flags = @import("flags.zig");
 
 const MEMORY_MAX = 1 << 16;
 var memory: [MEMORY_MAX]u16 = undefined;
 
 pub fn main() !void {
 
-
+    registers.reg[registers.Register.R_COND] = flags.Flags.FL_ZRO;
 }
 
 test "simple test" {
